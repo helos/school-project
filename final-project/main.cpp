5e8fcb5f14b3b40e96dc10ae6cf96ec8ec702507
mschueth <mschueth3@mail.gatech.edu>
2012-04-21 21:08:49 -0400

    getting set up

--- final-project/main.cpp
+++ final-project/main.cpp
@@ -3,18 +3,10 @@
 #include "grammer.h"
 #include "scanner.h"
 
-//Testing merging and stuff
-//testing it again --- michael
-
-/**********
-Comments*
-***********/
-
 using namespace std;
 
 int main() {
 	printf("hello world");
 	getchar();
 	return 0;
-	//I was
 }
---
+++ final-project/parsingTable.h
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+#ifndef __PARSING__
+#define __PARSING__
+
+
+#endif
