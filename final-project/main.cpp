184ca57b403c6d06cd529dc2774fc87368bd793c
Blake Palacio <helios2112@gmail.com>
2012-04-26 18:33:04 -0400

    removed some lines

--- final-project/main.cpp
+++ final-project/main.cpp
@@ -8,9 +8,7 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-	cout << "Opening " << argv[1] << endl;
 	ifstream grammerFile(argv[1], ifstream::in);
-	cout << "Opened  " << argv[1] << endl;
 	
 	readGrammer(&grammerFile);
 
