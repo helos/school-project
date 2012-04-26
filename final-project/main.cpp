5619191daca75898c5d2b23ad8e292ca4b5058cd
mschueth <mschueth3@mail.gatech.edu>
2012-04-26 19:11:55 -0400

    fixed printthegrammer

--- final-project/main.cpp
+++ final-project/main.cpp
@@ -32,17 +32,6 @@ void printTheGrammer(){
 	for(int i = 0; i < nonterminals.size(); i++)
 		for(int j=0; j<nonterminals[i]->rules.size(); j++)
 			cout<< nonterminals[i]->toString(j) << endl;
-	cout << "Terminals: "; 
-	for(int i = 0; i < terminals.size(); i++){
-		if(i%5==0) cout << endl << "\t";
-		cout<< terminals[i]->toString() << "\t";
-	}
-	cout << endl << endl;
-
-	cout << "NonTerminals: " << endl; 
-	for(int i = 0; i < nonterminals.size(); i++)
-		for(int j=0; j<nonterminals[i]->rules.size(); j++)
-			cout<< nonterminals[i]->toString(j) << endl;
 		
 
 }
